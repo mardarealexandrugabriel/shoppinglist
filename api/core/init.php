38096c6db23ef537d2a2338c714cf470ea06d992
Mardare <AMardare@romania.luxoft.com>
2016-10-28 13:08:33 +0300

    Conexiune+globals

---
+++ api/core/init.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+$GLOBALS['config'] = array(
+    'mysql'     => array(
+        'host'          => 'mysql.hostinger.ro',
+        'username'      => 'u956180736_user',
+        'password'      => 'u956180736_user',
+        'db'            => 'u956180736_shopp'
+    ),
+    'remember'  => array(
+        'cookie_name'   => 'hash',
+        'cookie_expiry' => 604800
+    ),
+    'session'   => array(
+        'session_name'  => "user",
+        'token_name'    => "token"
+    )
+);
+
+spl_autoload_register(function($class){
+    require_once 'classes/'.$class.'.php';
+});
+
+require_once 'functions/sanitize.php';
+
