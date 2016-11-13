745f0bfae93abbc8470e378492e421f5678d45dc
mardarealexandrugabriel <mardarealexandrugabriel@gmail.com>
2016-11-13 12:38:08 +0200

    test

--- api/index.php
+++ api/index.php
@@ -1,6 +1,6 @@
 <?php
     require_once('core/init.php');
-
+    //test
     $action = Input::get("action");
     header('Content-Type: application/json');
     header('Access-Control-Allow-Origin: *');
